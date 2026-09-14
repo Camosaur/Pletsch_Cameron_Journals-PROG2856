7dd29d3139c96aa0002e70b0f9c10746428e552d
Camosaur <cameronpletsch@gmail.com>
2026-09-13 21:06:27 -0400

    J1T3 complete
    You can now draw lines. It will calculate the total magnitude of each line for you!

---
+++ Assets/Week 1/Scripts/Pipeline.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Pipeline : MonoBehaviour
+{
+    Coroutine drawing;
+
+    public float totalMag = 0;
+    public Vector2 mousePos;
+
+
+    void Update()
+    {
+        //If mouse was pressed THIS FRAME, start/reset the coroutine
+        if (Mouse.current.leftButton.wasPressedThisFrame)
+        {
+            drawing = StartCoroutine(DrawAndCalculate());
+        }
+    }
+
+    IEnumerator DrawAndCalculate() {
+        //Initialize a "TotalMag" float variable
+
+
+        //While true loop...
+        while (true)
+        {
+            //Store mouse position
+            mousePos = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+
+            //Wait 0.1 seconds
+            yield return new WaitForSeconds(0.1f);
+
+            //If the mouse is not held down, print TotalMag and then exit the coroutine
+            //yield break;
+            if (!Mouse.current.leftButton.isPressed) {
+                Debug.Log(totalMag);
+                yield break;
+            }
+
+            //Draw the line
+            Debug.DrawLine(mousePos, Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue()), Random.ColorHSV(), 99999999);
+
+            //Find the vector between the current mouse position and the previous one
+            Vector2 between = mousePos - (Vector2)Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+
+            //Add that vectors magnatude to TotalMag
+            totalMag += Mathf.Abs(Mathf.Sqrt(Mathf.Pow(between.x, 2) + Mathf.Pow(between.x, 2)));
+        }
+    }
+}
